f16d94278e5789979460c6eae7c8fc3cb4d84cea
hqxqyly <hqxfhlx@163.com>
2018-08-12 14:41:27 +0800

    ss

---
+++ src/ums/src/main/java/com/qiyue/ums/Application.java
@@ -0,0 +1,18 @@
+package com.qiyue.ums;
+
+/**
+ * 启动类
+ * 
+ * @author Qiaoxin.Hong
+ *
+ */
+public class Application {
+	
+	/**
+	 * 启动方法
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		System.out.println("==========");
+	}
+}
